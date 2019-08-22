b49b123a0dd4b7af2924a17480343cc336821a89
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-22 15:29:23 -0600

    movie selected for edit now populates form

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const UpdateMovie = ({ savedList, setSavedList }) => {
-    const[movie, setMovie] = useState({ title: '', director: '', metascore: '', stars: [] })
+const UpdateMovie = (props) => {
+    const[movies, setMovies] = useState({ title: '', director: '', metascore: '', stars: [] })
 
-    // useEffect(() => {
-    //     const id = props.match.params.id;
-    //     const itemInMovie = props.
-    // }, [])
+    useEffect(() => {
+        const id = props.match.params.id;
+        const itemInMovie = props.movie.find(mov => `${mov.id}` === id)
+        if (itemInMovie) setMovies(itemInMovie)
+    }, [props.movie , props.match.params.id])
 
     const changeHandler = event => {
         event.persist();
-        setMovie({
-            ...movie,
+        setMovies({
+            ...movies,
             [event.target.name]: event.target.value
         })
     }
@@ -20,7 +21,7 @@ const UpdateMovie = ({ savedList, setSavedList }) => {
     const handleSubmit = event => {
         event.preventDefault();
         axios
-            .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+            .put(`http://localhost:5000/api/movies/${movies.id}`, movies)
             .then(res => console.log("res from update movie", res))
             .catch(err => console.log(err.response))
     }
@@ -29,10 +30,10 @@ const UpdateMovie = ({ savedList, setSavedList }) => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type='text' name='title' value={movie.title} placeholder='Title' onChange={changeHandler} />
-            <input type='text' name='director' value={movie.director} placeholder='Director' onChange={changeHandler} />
-            <input type='text' name='metascore' value={movie.metascore} placeholder='metascore' onChange={changeHandler} />
-            <input type='text' name='stars' value={movie.stars} placeholder='Stars' onChange={changeHandler} />
+            <input type='text' name='title' value={movies.title} placeholder='Title' onChange={changeHandler} />
+            <input type='text' name='director' value={movies.director} placeholder='Director' onChange={changeHandler} />
+            <input type='text' name='metascore' value={movies.metascore} placeholder='metascore' onChange={changeHandler} />
+            <input type='text' name='stars' value={movies.stars} placeholder='Stars' onChange={changeHandler} />
             <button>Update</button>
         </form>
     )

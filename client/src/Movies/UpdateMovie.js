febbe84b99b343dcab6cc07123a7b8af28d96641
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-22 13:53:45 -0600

    working on UpdateMovie.js

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 
 const UpdateMovie = ({}) => {
+    const[movie, setMovie] = useState({ title: '', director: '', metascore: '', stars: [] })
+
+    
     return (
         <h1>Update Movie</h1>
     )

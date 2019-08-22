7675f160d063d6bf75d7c60eab9d17220849c108
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-22 13:57:26 -0600

    working on updatemovie form

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -3,9 +3,15 @@ import React, { useState, useEffect } from 'react'
 const UpdateMovie = ({}) => {
     const[movie, setMovie] = useState({ title: '', director: '', metascore: '', stars: [] })
 
-    
+
+
     return (
-        <h1>Update Movie</h1>
+        <form>
+            <input type='text' name='title' value={movie.title} placeholder='Title' onChange={} />
+            <input type='text' name='director' value={movie.director} placeholder='Director' onChange={} />
+            <input type='text' name='metascore' value={movie.metascore} placeholder='metascore' onChange={} />
+            <input type='text' name='stars' value={movie.stars} placeholder='Stars' onChange={} />
+        </form>
     )
 }
 
